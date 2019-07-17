a7a162f731130724c756f2ecc6248d59a2d2c5dd
Thomas Ferro <t.ferro184@gmail.com>
2019-07-17 18:19:07 +0200

    feat (path) : Manage src path via env variable

--- src/main.go
+++ src/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"os"
+
 	"github.com/go-dev-env/triggers"
 	"github.com/go-dev-env/modules"
 	"github.com/go-dev-env/builders/docker"
 )
 
+func getPath() string {
+	path := os.Getenv("SRC_PATH")
+	if path == "" {
+		path = "/src"
+	}
+	return path
+}
+
 func main() {
-	// TODO : Se baser sur la config / les paramètres de la ligne de commande
-	path := "."
+	path := getPath()
+	
 	trigger := triggers.NewFileWatcherTrigger(path)
 	builder := docker.NewBuilder()
 
